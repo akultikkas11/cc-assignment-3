8c0e03c512d4313e8b85f7fea72b0e9caaf332e5
akultikkas11 <akultikkas11@gmail.com>
2025-10-30 23:52:56 +0530

    msg2

--- backend/db.js
+++ backend/db.js
@@ -4,7 +4,7 @@ const db = mysql.createConnection({
     host: 'photo-gallery-db.cbkecoy4abv6.eu-north-1.rds.amazonaws.com',
     user: 'admin',
     password: 'database',
-    database: 'photo_gallery'
+    database: 'Student'
 });
 
 db.connect((err)=>{
